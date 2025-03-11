cb4c36f72eedede067e38efe2c3afdd53f4e613f
NocklasBalten <niklas.duisburg@freenet.de>
2025-03-11 20:50:30 +0000

    cleaned up comments, removed io module usage and in main reduced indentation by inverting if conditions

--- extract_limbach_pdf.py
+++ extract_limbach_pdf.py
@@ -1,4 +1,3 @@
-from io import StringIO
 import sys
 import json
 from pdfminer.converter import PDFPageAggregator
@@ -9,6 +8,7 @@ from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 
 def main():
+    
     # Validate the pdf_path and output_path
     if len(sys.argv) != 3:
         print("Usage: extract_limbach_pdf.py [input.pdf] [output.txt]")
@@ -20,22 +20,21 @@ def main():
         sys.exit(1)
 
     output_path = sys.argv[2]
-    if output_path[-4:] != '.txt':
-        print("Error: The output file must be a TXT file.")
+    if output_path[-5:] != '.json':
+        print("Error: The output file must be a JSON file.")
         sys.exit(1)
 
-    output_string = StringIO()
     lab_results = [] # list of json formatted outputs per page
+
     with open(input_path, 'rb') as in_file:
+
+        # Parse and read the PDF file
         parser = PDFParser(in_file)
         doc = PDFDocument(parser)
         rsrcmgr = PDFResourceManager()
         device = PDFPageAggregator(rsrcmgr, laparams=LAParams())
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        page_number = 0
+        interpreter = PDFPageInterpreter(rsrcmgr, device)        
         for page in PDFPage.create_pages(doc):
-            page_number += 1
-            output_string.write(f"Page number: {page_number}\n")
             interpreter.process_page(page)
             layout = device.get_result()
 
@@ -47,57 +46,51 @@ def main():
                         text_lines.append(text_line)
 
             # Extract patient infos and lab results
+
             # Initialize json_output
-            lab_result = {} #json formatted output per page
-            lab_result["Parameters"] = []
+            lab_result = {} # json formatted output per page
+            lab_result["Parameters"] = [] # Ensure "Parameters" is a list
+
             for text_line in text_lines:
                 x0, y0, _, _ = text_line.bbox  # Coordinates of the text
-                if y0 >= 514 and y0 <= 515:  # Check if the text is in the range of the patient infos
-                #    output_string.write(extract_patient_infos(text_line, x0))
-                    lab_result.update(extract_patient_infos(text_line, x0))
-                elif y0 < 461:  # Check if the text is in the range of the lab results, beneath "Untersuchung"
-                #    output_string.write(extract_lab_results(text_line, x0, y0, text_lines))
-                    parameter = extract_lab_results(text_line, x0, y0, text_lines)
-                    if parameter: # Ensure that the parameter is not empty
-                        if "Comment" in parameter:
-                            try:
-                                if "Comment" in lab_result["Parameters"][-1]:
-                                    lab_result["Parameters"][-1]["Comment"] = lab_result["Parameters"][-1]["Comment"] + '\n' + parameter["Comment"]
-                                else:
-                                    lab_result["Parameters"][-1].update(parameter)
-                            except IndexError:
-                                lab_result["Parameters"].append(parameter)
-                        else:
-                            lab_result["Parameters"].append(parameter)
-                    # lab_result["Parameters"] = (extract_lab_results(text_line, x0, y0, text_lines))
-            lab_results.append(lab_result)
-            # print(page_number)
-            # print(json.dumps(lab_results, ensure_ascii=False, indent=4))
 
-    # Write the extracted text to the output file
-    lines = output_string.getvalue().split('\n')
-    i = 0
-    # Combine multi-line comments into one string
-    for line in lines:
-        if lines[i].startswith("Comment: "):
-            while lines[i + 1].startswith("Comment: "):
-                lines[i] = lines[i] + '\n' + lines[i + 1][9:]
-                lines.pop(i + 1)
-           #     json_output["Comment"] = json.dumps(lines[i], ensure_ascii=False)
-           #     print(json_output)
-        # print(f"{i}: {lines[i]}")
-        i += 1
-    
+                if y0 >= 514 and y0 <= 515:  # Check if the text is in the range of the patient infos
+                    lab_result.update(extract_patient_infos(text_line, x0))
+
+                elif y0 < 461:  # Check if the text is in the range of the lab results, beneath "Untersuchung"
+                    parameter = extract_lab_results(text_line, x0, y0, text_lines)
+
+                    # Comments need extra handling because they can span multiple lines
+
+                    if not parameter:
+                        continue
+                
+                    # if parameter is not a comment, its key(s) and value(s) can be added to the previous parameter
+                    # in the list of parameters
+
+                    if "Comment" not in parameter:
+                        lab_result["Parameters"].append(parameter)
+                        continue
+
+                    try:
+                        # if the previous parameter already has a comment, append the current comment to it
+                        if "Comment" in lab_result["Parameters"][-1]:
+                            lab_result["Parameters"][-1]["Comment"] = lab_result["Parameters"][-1]["Comment"] + '\n' + parameter["Comment"]
+                        # if the previous parameter does not have a comment, add the current comment to it
+                        else:
+                            lab_result["Parameters"][-1].update(parameter)
+                    # for multi-page lab results, the first parameter may be a comment, which would raise an IndexError
+                    except IndexError:
+                        lab_result["Parameters"].append(parameter)
+                
+            lab_results.append(lab_result)
+
     # clean up the text
     lab_results = json.dumps(lab_results, ensure_ascii=False, indent = 4) # json formatted output
     lab_results = lab_results.replace('(cid:13)', '')
 
     with open(output_path, 'w') as output_file:
         output_file.write(lab_results)
-        
-        
-
-
 
 def extract_patient_infos(text_line, x0):
     output = {}
@@ -115,7 +108,7 @@ def extract_patient_infos(text_line, x0):
         birth_gender = text_line.get_text().strip()
         birth_gender_split = birth_gender.split('/')
         if len(birth_gender_split) == 2:
-            birthday = birth_gender_split[0]
+            birthday = birth_gender_split[0].strip() #strips the whitespace before the slash
             gender = birth_gender_split[1].strip() #strips the whitespace after the slash
             output["Geburtsdatum"] = birthday
             output["Geschlecht"] = gender
